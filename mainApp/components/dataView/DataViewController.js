b2e27d2e7f997244b1c6b9d3260a5e04b0e6caf9
David Slater <dsslater@stanford.edu>
2016-06-18 14:31:20 -0700

    changed to test framework for graph

--- mainApp/components/dataView/DataViewController.js
+++ mainApp/components/dataView/DataViewController.js
@@ -1,5 +1,7 @@
 ANBIOApp.controller('DataViewController', ['$scope','$location', '$http', '$routeParams',
   function ($scope, $location, $http, $routeParams) {
+      $scope.numRows = 1;
+      $scope.numCols = 1;
       $scope.counter = function(n) {
         var arr = new Array(n);
         return arr;
--- mainApp/components/graph/GraphController.js
+++ mainApp/components/graph/GraphController.js
@@ -1,6 +1,7 @@
 ANBIOApp.controller('GraphController', ['$scope','$location', '$http', '$routeParams',
-  function ($scope, $location, $http, $routeParams) {
-      $scope.FetchData = function(url, doneCallback){
+    function ($scope, $location, $http, $routeParams) {
+
+        $scope.FetchData = function(url, doneCallback){
             var xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function(){
                 if (this.readyState != 4){ 
@@ -12,8 +13,25 @@ ANBIOApp.controller('GraphController', ['$scope','$location', '$http', '$routePa
                  }
                  doneCallback(this.responseText);
             };
-            
+
             xhr.open("GET", url);
             xhr.send();  
         }
+        var connection = new WebSocket('ws://localhost:8000', ['soap', 'xmpp']);
+        // When the connection is open, send some data to the server
+        connection.onopen = function () {
+            connection.send('Ping'); // Send the message 'Ping' to the server
+            console.log('WebSocket Connected')
+        };
+
+        // Log errors
+        connection.onerror = function (error) {
+            console.log('WebSocket Error ' + error);
+        };
+
+        // Log messages from the server
+        connection.onmessage = function (e) {
+            console.log('Server: ' + e.data);
+        };
+
   }]);
