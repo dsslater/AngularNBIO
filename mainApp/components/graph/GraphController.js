d9f286b4229cbcfa2c28acce7e473412c589ca22
David Slater <dsslater@stanford.edu>
2016-06-18 13:16:57 -0700

    Merged Trav's changes

--- mainApp/components/graph/GraphController.js
+++ mainApp/components/graph/GraphController.js
@@ -1,4 +1,19 @@
 ANBIOApp.controller('GraphController', ['$scope','$location', '$http', '$routeParams',
   function ($scope, $location, $http, $routeParams) {
-      
+      $scope.FetchData = function(url, doneCallback){
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function(){
+                if (this.readyState != 4){ 
+                    return; 
+                 }
+                 //Final State but status not OK
+                 if (this.status != 200) {
+                    return;
+                 }
+                 doneCallback(this.responseText);
+            };
+            
+            xhr.open("GET", url);
+            xhr.send();  
+        }
   }]);
